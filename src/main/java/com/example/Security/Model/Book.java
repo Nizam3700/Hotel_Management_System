e4e100cae08770b4dd1da26950abd9f6c40f8476
Nizamuddin <nizamuddin8637@gmail.com>
2024-08-31 14:51:57 +0530

    deploy

--- src/main/java/com/example/Security/Model/Book.java
+++ src/main/java/com/example/Security/Model/Book.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 public class Book {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @ManyToOne
